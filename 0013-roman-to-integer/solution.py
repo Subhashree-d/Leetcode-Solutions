75927a934c24d3c066cb54fa04bf1db1612b1d02
Subhashree-d <subhadshree10@gmail.com>
2024-05-05 14:10:10 +0000

    Sync LeetCode submission Runtime - 43 ms (5.16%), Memory - 16.6 MB (100.00%)

---
+++ 0013-roman-to-integer/solution.py
@@ -0,0 +1,15 @@
+
+       # roman = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+class Solution:
+    def romanToInt(self, S: str) -> int:
+        
+        roman = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+        summ= 0
+        for i in range(len(S)-1,-1,-1):
+            num = roman[S[i]]
+            if 3*num < summ: 
+                summ = summ-num
+            else: 
+                summ = summ+num
+        return summ
+        
