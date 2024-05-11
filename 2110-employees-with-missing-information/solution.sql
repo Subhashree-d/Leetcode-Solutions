d44b5f4d927cb4765d454d5d959de1ac9c691b94
Subhashree-d <subhadshree10@gmail.com>
2024-05-11 05:31:01 +0000

    Sync LeetCode submission Runtime - 1226 ms (5.05%), Memory - 0.0B (100.00%)

---
+++ 2110-employees-with-missing-information/solution.sql
@@ -0,0 +1,12 @@
+select e.employee_id
+    from Employees e
+    left join Salaries s
+    ON s.employee_id = e.employee_id
+    where s.salary is null
+union all
+select s.employee_id
+    from Employees e
+    right join Salaries s
+    ON s.employee_id = e.employee_id
+    where e.name is null
+order by employee_id;
