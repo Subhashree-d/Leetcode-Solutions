525b0b6e46f4f38e45d14a4d34cff59b27df3655
Subhashree-d <subhadshree10@gmail.com>
2024-06-17 06:01:45 +0000

    Sync LeetCode submission Runtime - 18 ms (0.32%), Memory - 11.5 MB (100.00%)

---
+++ 0070-climbing-stairs/solution.py
@@ -0,0 +1,15 @@
+class Solution(object):
+ def climbStairs(self, n):
+    prev= 1
+    prev2= 1
+    # Running for loop to count all possible ways
+    for i in range(2, n+1):
+        curr = prev + prev2
+        prev2 = prev
+        prev = curr
+    return prev
+
+
+
+
+        
