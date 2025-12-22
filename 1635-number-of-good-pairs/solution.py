b2a03d35ebf8d308a0354ce51957b67adf758832
Subhashree-d <subhadshree10@gmail.com>
2025-12-22 08:59:56 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.2 MB (100.00%)

---
+++ 1635-number-of-good-pairs/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def numIdenticalPairs(self, nums: List[int]) -> int:
+        c=0
+        for i in range(len(nums)):
+           for j in range(1, len(nums)):
+                if nums[i]==nums[j] and i<j:
+                    c=c+1
+        return c
+                 
+
+
+        
