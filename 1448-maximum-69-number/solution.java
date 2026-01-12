16dc5f667d862b6b2eaff97df92c5931f3de55e1
Subhashree-d <subhadshree10@gmail.com>
2026-01-12 14:24:49 +0000

    Sync LeetCode submission Runtime - 1 ms (79.55%), Memory - 42 MB (91.52%)

---
+++ 1448-maximum-69-number/solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maximum69Number (int num) {
+        char[] d=String.valueOf(num).toCharArray();
+        for(int i=0;i<d.length;i++){
+            if(d[i]=='6'){
+                d[i]='9';break;
+
+            }
+        }
+        return Integer.parseInt(new String(d));
+        
+        
+    }
+}
