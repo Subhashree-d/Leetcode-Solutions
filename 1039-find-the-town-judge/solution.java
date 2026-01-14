167c4730a7591b7e3742f19c1870bebd294b1bb9
Subhashree-d <subhadshree10@gmail.com>
2026-01-14 05:38:56 +0000

    Sync LeetCode submission Runtime - 3 ms (73.54%), Memory - 53.9 MB (20.23%)

---
+++ 1039-find-the-town-judge/solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int findJudge(int n, int[][] trust) {
+        int []c=new int[n+1];
+        for(int[]t:trust){
+            c[t[0]]--;
+            c[t[1]]++;
+        }
+        for (int i = 1; i <= n; i++) {
+    if (c[i] == n - 1) return i;
+}
+return -1;
+        
+    }
+}
