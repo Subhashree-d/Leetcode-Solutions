ce2958c28a6989c4010534ee2ed96070a5ae03f2
Subhashree-d <subhadshree10@gmail.com>
2026-01-18 07:41:08 +0000

    Sync LeetCode submission Runtime - 49 ms (6.84%), Memory - 48.2 MB (8.66%)

---
+++ 0260-single-number-iii/solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] singleNumber(int[] nums) {
+        int n=nums.length;
+        int[]res=new int[2];
+        int index=0;
+        for(int i=0;i<n;i++){
+            boolean found=false;
+            for(int j=0;j<n;j++){
+                if(i!=j && nums[i]==nums[j]){
+                    found=true;
+                    break;
+                }
+            }
+            if(!found){
+                res[index++]=nums[i];
+                if(index==2)break;
+            }
+        }
+        return res;
+        
+    }
+}
