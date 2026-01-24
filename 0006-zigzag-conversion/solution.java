3f46397ea913d16908b0b638fc2311824b1f9ff3
Subhashree-d <subhadshree10@gmail.com>
2026-01-24 09:02:45 +0000

    Sync LeetCode submission Runtime - 5 ms (54.34%), Memory - 47 MB (25.56%)

--- 0006-zigzag-conversion/solution.java
+++ 0006-zigzag-conversion/solution.java
@@ -1,32 +1,61 @@
 class Solution {
     public String convert(String s, int numRows) {
-        if (numRows == 1) {
+        // if (numRows == 1) {
+        //     return s;
+        // }
+        
+        // StringBuilder answer = new StringBuilder();
+        // int n = s.length();
+        // int diff = 2 * (numRows - 1);
+        // int diagonalDiff = diff;
+        // int secondIndex;
+        // int index;
+        // for (int i = 0; i < numRows; i++) {
+        //     index = i;
+
+        //     while (index < n) {
+        //         answer.append(s.charAt(index));
+        //         if (i != 0 && i != numRows - 1) {
+        //             diagonalDiff = diff-2*i;
+        //             secondIndex = index + diagonalDiff;
+                    
+        //             if (secondIndex < n) {
+        //                 answer.append(s.charAt(secondIndex));
+        //             }
+        //         }
+        //         index += diff;
+        //     }
+        // }
+        
+        // return answer.toString();
+        if(numRows==1 || s.length()<numRows){
             return s;
         }
-        
-        StringBuilder answer = new StringBuilder();
-        int n = s.length();
-        int diff = 2 * (numRows - 1);
-        int diagonalDiff = diff;
-        int secondIndex;
-        int index;
-        for (int i = 0; i < numRows; i++) {
-            index = i;
 
-            while (index < n) {
-                answer.append(s.charAt(index));
-                if (i != 0 && i != numRows - 1) {
-                    diagonalDiff = diff-2*i;
-                    secondIndex = index + diagonalDiff;
-                    
-                    if (secondIndex < n) {
-                        answer.append(s.charAt(secondIndex));
-                    }
-                }
-                index += diff;
-            }
+        StringBuilder[]rows=new StringBuilder[numRows];
+        for(int i=0;i<numRows;i++){
+            rows[i]=new StringBuilder();
         }
+
+        int in=0;
+        int st=1;
+        for(char c:s.toCharArray()){
+            rows[in].append(c);
+            if(in==0){
+                st=1;
+            }
+            else if(in==numRows-1){
+                st=-1;
+            }
+            in+=st;
+        }
+        StringBuilder res=new StringBuilder();
+        for (StringBuilder c:rows){
+            res.append(c);
+        }
+        return res.toString();
         
-        return answer.toString();
+
+        
     }
 }
