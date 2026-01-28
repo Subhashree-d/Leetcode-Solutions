6f2cb9932a38e9f3b962bc720ad69c5f5ca8f312
Subhashree-d <subhadshree10@gmail.com>
2026-01-28 05:54:06 +0000

    Sync LeetCode submission Runtime - 6 ms (49.21%), Memory - 55.9 MB (17.50%)

--- 0169-majority-element/solution.java
+++ 0169-majority-element/solution.java
@@ -1,8 +1,11 @@
 class Solution {
     public int majorityElement(int[] nums) {
-       Arrays.sort(nums);
-       int num=nums.length;
-       return nums[num/2];
+    //    Arrays.sort(nums);
+    //    int num=nums.length;
+    //    return nums[num/2];
+    Arrays.sort(nums);
+    int num=nums.length;
+    return nums[num/2];
     
     }
 }
