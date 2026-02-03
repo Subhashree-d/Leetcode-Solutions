61cfdf7163675658a85207936119491c1e7dba33
Subhashree-d <subhadshree10@gmail.com>
2026-02-03 17:32:51 +0000

    Sync LeetCode submission Runtime - 1 ms (17.13%), Memory - 48.4 MB (13.52%)

--- 0034-find-first-and-last-position-of-element-in-sorted-array/solution.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/solution.java
@@ -1,61 +1,15 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-     
-        return new int[] {first(nums, target )  , last(nums , target)};
-    }
-
-     
-    private int first(int [] arr , int target){
-        
-        int left = 0 ; 
-        int right = arr.length -1; 
-        
-        int ans = -1;
-
-        while(left<=right){
-            int mid = left + (right - left) / 2;
-
-            
-          
-            if(arr[mid] == target ){
-
-                
-                ans = mid;
-               
-                right = mid - 1;
-            }
-           
-            else if (arr[mid]<target){
-                left = mid + 1; 
-            }
-        
-            else{
-                right = mid -1 ; 
+        int arr[]={-1,-1};
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==target){
+                if(arr[0]==-1){
+                    arr[0]=i;
+                }
+                arr[1]=i;
             }
         }
-        return ans ; 
-
-    }
-
-
-    private int last (int [] arr , int target){
-        int left = 0; 
-        int right = arr.length-1;
-        int ans = -1;
-
-        while(left <= right ){
-            int mid = left + (right - left )/2; 
-
-            if (arr[mid] == target ){
-                ans = mid ;
-                left = mid+1 ; 
-            }
-            else if (arr[mid]>target ){
-                right = mid -1;
-            }else{
-                left = mid + 1; 
-            }
-        }
-        return ans ;
+        return arr;
+        
     }
 }
