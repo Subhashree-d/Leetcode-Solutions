ac324da2db3f6d42dca4ae032765b2e8fc50520d
Subhashree-d <subhadshree10@gmail.com>
2026-02-18 16:06:07 +0000

    Sync LeetCode submission Runtime - 20 ms (17.53%), Memory - 48.3 MB (5.53%)

---
+++ 1786-count-the-number-of-consistent-strings/solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int countConsistentStrings(String allowed, String[] words) {
+         int count = 0;
+
+    for (String word : words) {
+        boolean isValid = true;
+
+        for (char ch : word.toCharArray()) {
+            if (!allowed.contains(String.valueOf(ch))) {
+                isValid = false;
+                break;
+            }
+        }
+
+        if (isValid) count++;
+    }
+
+    return count;
+    }
+}
