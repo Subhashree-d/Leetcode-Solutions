d77ea105ced0d1fc45fd89975c85a660f5bf9515
Subhashree-d <subhadshree10@gmail.com>
2026-02-28 15:28:01 +0000

    Sync LeetCode submission Runtime - 5 ms (64.27%), Memory - 47.2 MB (17.58%)

---
+++ 4231-smallest-pair-with-different-frequencies/solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] minDistinctFreqPair(int[] nums) {
+        HashMap<Integer,Integer>map=new HashMap<>();
+        for(int num:nums){
+            map.put(num,map.getOrDefault(num,0)+1);
+        }
+        List<Integer>list=new ArrayList<>(map.keySet());
+        Collections.sort(list);
+        for(int i=0;i<list.size();i++){
+            for(int j=i+1;j<list.size();j++){
+                int x=list.get(i);
+                int y=list.get(j);
+                if(!map.get(x).equals(map.get(y))){
+                    return new int[]{x,y};
+                }
+            }
+        }
+        return new int[]{-1,-1};
+    }
+}
