146f598321b3f816495d236e5a35a5112a162f78
Subhashree-d <subhadshree10@gmail.com>
2026-03-26 06:59:16 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 42.9 MB (43.27%)

--- 0059-spiral-matrix-ii/solution.java
+++ 0059-spiral-matrix-ii/solution.java
@@ -1,21 +1,34 @@
 class Solution {
     public int[][] generateMatrix(int n) {
-        int x = 0, y = 0, dx = 1, dy = 0;
-        int[][] res = new int[n][n];
-
-        for (int i = 0; i < n * n; i++) {
-            res[y][x] = i + 1;
-
-            if (!(0 <= x + dx && x + dx < n && 0 <= y + dy && y + dy < n && res[y+dy][x+dx] == 0)) {
-                int temp = dx;
-                dx = -dy;
-                dy = temp;
+        int res[][]=new int[n][n];
+        int top=0;
+        int bottom=n-1;
+        int left=0;
+        int right=n-1;
+        int num=1;
+        while(left<=right && top<=bottom){
+           for(int j=left;j<=right;j++){
+            res[top][j]=num++;
+           }
+           top++;
+           for(int i=top;i<=bottom;i++){
+            res[i][right]=num++;
+           }
+           right--;
+           if(left<=right){
+            for(int j=right;j>=left;j--){
+                res[bottom][j]=num++;
             }
-            
-            x += dx;
-            y += dy;
+            bottom--;
+           }
+           if(top<=bottom){
+            for(int i=bottom;i>=top;i--){
+                res[i][left]=num++;
+            }
+            left++;
+           }
         }
-
-        return res;        
+        return res;
+        
     }
 }
