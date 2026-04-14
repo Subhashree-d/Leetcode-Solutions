bc05a70e6471a6519f927f3fc6891e57107fb86f
Subhashree-d <subhadshree10@gmail.com>
2026-04-14 02:55:48 +0000

    Sync LeetCode submission Runtime - 2 ms (99.38%), Memory - 47.4 MB (10.91%)

--- 0001-two-sum/solution.java
+++ 0001-two-sum/solution.java
@@ -1,23 +1,37 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        // for (int i = 0; i < nums.length; i++) {
-        //     for (int j = 0; j < nums.length; j++) {
-        //         if (i != j && nums[i] + nums[j] == target) {
-        //             return new int[]{i, j};
+        // // for (int i = 0; i < nums.length; i++) {
+        // //     for (int j = 0; j < nums.length; j++) {
+        // //         if (i != j && nums[i] + nums[j] == target) {
+        // //             return new int[]{i, j};
+        // //         }
+        // //     }
+        // // }
+        // // return new int[]{-1, -1};
+
+        // int n=nums.length;
+        // for(int i=0;i<n-1;i++){
+        //     for(int j=i+1;j<n;j++){
+        //         if(nums[i]+nums[j]==target){
+        //             return new int[]{i,j};
+
         //         }
         //     }
         // }
-        // return new int[]{-1, -1};
+        // return new int[]{};
 
-        int n=nums.length;
-        for(int i=0;i<n-1;i++){
-            for(int j=i+1;j<n;j++){
-                if(nums[i]+nums[j]==target){
-                    return new int[]{i,j};
+        HashMap<Integer,Integer>map=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            int complement=target-nums[i];
+            if(map.containsKey(complement)){
+                return new int[]{map.get(complement),i};
 
-                }
             }
+            map.put(nums[i],i);
         }
         return new int[]{};
+       
+
+        
     }
 }
