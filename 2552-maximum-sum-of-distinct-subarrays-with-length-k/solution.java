c96d9609e0ea49c63181daad240d212e3860b3e0
Subhashree-d <subhadshree10@gmail.com>
2026-05-18 12:10:12 +0000

    Sync LeetCode submission Runtime - 56 ms (39.93%), Memory - 99.2 MB (45.32%)

---
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public long maximumSubarraySum(int[] nums, int k) {
+        Map<Integer,Integer>map=new HashMap<>();
+        long sum=0,maxsum=0;
+        int left=0;
+        for(int right=0;right<nums.length;right++){
+            map.put(nums[right],map.getOrDefault(nums[right],0)+1);
+            sum+=nums[right];
+            if(right-left+1>k){
+                 map.put(nums[left], map.get(nums[left]) - 1);
+            if(map.get(nums[left]) == 0) map.remove(nums[left]);
+            sum -= nums[left];
+            left++;
+            }
+            if(map.size() == k){
+            maxsum = Math.max(maxsum, sum);
+        }
+        }
+        return maxsum;
+        
+
+
+    }
+}
